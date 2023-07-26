b4fe7990d90b3769157ba3e001390eccde9d4357
mechalr8 <harsitagrawal59@gmail.com>
2023-07-26 13:00:08 +0530

    First Commit

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.inventory
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.inventory.data.Item
 import com.example.inventory.data.ItemDao
@@ -9,10 +11,39 @@ import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
 
 class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+    val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
     private fun insertItem(item: Item) {
         viewModelScope.launch {
             itemDao.insert(item)
         }
+    }
+
+    fun isStockAvailable(item: Item): Boolean{
+        return (item.quantityInStock > 0)
+    }
+
+    fun updateItem(item: Item){
+        viewModelScope.launch {
+            itemDao.update(item)
+        }
+    }
+
+    fun sellItem(item: Item){
+        if(item.quantityInStock > 0){
+            val newItem = item.copy(quantityInStock = item.quantityInStock - 1)
+            updateItem(newItem)
+        }
+    }
+
+    fun deleteItem(item: Item){
+        viewModelScope.launch {
+            itemDao.delete(item)
+        }
+    }
+
+    fun retrieveItem(id:Int): LiveData<Item> {
+        return itemDao.getItem(id).asLiveData()
     }
 
     private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item{
@@ -32,6 +63,30 @@ class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
         if(itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank())
             return false
         return true
+    }
+
+    private fun getUpdatedItemEntry(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ): Item {
+        return Item(
+            id = itemId,
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    fun updateItem(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ){
+        val updatedItem = getUpdatedItemEntry(itemId, itemName, itemPrice, itemCount)
+        updateItem(updatedItem)
     }
 }
 
